d4198cbc82f18ccd5cb55438becbbf386ccb87db
sucongohu <1126045770@qq.com>
2022-06-20 13:00:19 +0800

    fix: defaultTransport

--- util/proxy.go
+++ util/proxy.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"crypto/tls"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -14,23 +15,36 @@ import (
 // MakeClient based proxy
 func MakeClient(key string, timeout time.Duration) http.Client {
 	var (
-		conf      = os.Getenv(key)
+		conf             = os.Getenv(key)
+		defaultTransport = &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			Proxy:           http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		}
 		transport *http.Transport
 		err       error
 	)
 	if strings.HasPrefix(conf, "http") {
 		if transport, err = MakeHTTPProxy(conf); err != nil {
-			return http.Client{Timeout: timeout}
+			return http.Client{Timeout: timeout, Transport: defaultTransport}
 		}
 	} else if len(conf) > 1 {
 		if transport, err = MakeSocksProxy(conf, os.Getenv(key+"_USER"), os.Getenv(key+"_PASSWORD")); err != nil {
-			return http.Client{Timeout: timeout}
+			return http.Client{Timeout: timeout, Transport: defaultTransport}
 		}
 	}
 	if transport != nil {
-		return http.Client{Transport: transport, Timeout: timeout}
+		return http.Client{Timeout: timeout, Transport: transport}
 	}
-	return http.Client{Timeout: timeout}
+	return http.Client{Timeout: timeout, Transport: defaultTransport}
 }
 
 // MakeSocksProxy return socks proxy Transport
